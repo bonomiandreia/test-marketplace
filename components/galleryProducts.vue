865f85539358d3d3e33510299d23827c76a987c9
Andreia Bonomi <andreia@codeflame.io>
2025-06-08 13:54:24 +0100

    height

--- components/galleryProducts.vue
+++ components/galleryProducts.vue
@@ -1,23 +1,18 @@
 <template>
     <div>
-      <!-- 
-        Correction 1: Removed `grid-rows-2` from the main grid. 
-        This allows the single row's height to be determined naturally by its tallest content (the left column).
-      -->
       <div class="grid grid-cols-5 gap-4">
-        <!-- This left column's structure is fine as it defines the overall height -->
         <div class="col-span-4">
           <div class="grid grid-cols-7 grid-rows-2 gap-4">
             <div class="col-span-7 h-[500px]">
               <MainProduct :widget="mock"></MainProduct>
             </div>
-            <div class="col-span-2 row-start-2 h-[300px]">
+            <div class="col-span-2 row-start-2 h-[250px]">
               <MoreProducts :widget="mock"></MoreProducts>
             </div>
-            <div class="col-span-2 col-start-3 row-start-2 h-[300px]">
+            <div class="col-span-2 col-start-3 row-start-2 h-[250px]">
               <Downloads :widget="mock"></Downloads>
             </div>
-            <div class="col-span-3 col-start-5 row-start-2 h-[300px]">
+            <div class="col-span-3 col-start-5 row-start-2 h-[250px]">
               <PopularProduct :widget="mock"></PopularProduct>
             </div>
           </div>

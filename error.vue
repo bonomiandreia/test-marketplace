b5bc6e0412161dfee6dc7b887f66fc1f2f13038e
Andreia Bonomi <andreia@codeflame.io>
2025-06-10 09:38:33 +0100

    error back rome

--- error.vue
+++ error.vue
@@ -2,7 +2,14 @@
     <NuxtLayout name="404">
       <div class="container mx-auto px-4 text-center">
         <div class="text-4xl">Oops, wrong url page!</div>
-        <button class="font-bold button" @click="goBack">Back</button>
+        <button
+              @click="goBack"
+              class="group inline-flex items-center justify-center button-mindaro-marketplace p-2 mt-3"
+            >
+          <span class="text-center">
+            Back to home!
+          </span>
+        </button>
       </div>
     </NuxtLayout>
   </template>
